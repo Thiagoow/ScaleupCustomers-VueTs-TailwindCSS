4f70d06269b7a9b350c50160067b9c7fa127afd1
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-26 17:47:57 -0300

    Using Section component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <Header />
+
+  <main class="mx-5 xs:mx-14 lg:!mx-auto lg:max-w-5xl">
+    <Section
+      title="Informatize sua forma de educar"
+      text="Em pleno 2022, a tecnologia é o que mais desperta o interesse em nosso alunos. Ensine seus alunos de forma divertida e inovadora agora mesmo com a Scaleup!"
+      image="feed.png"
+      alt="Feed de vídeos curtos"
+    />
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Header from './components/molecules/Header.vue'
+import Section from './components/organisms/Section.vue'
 
 export default defineComponent({
   components: {
-    Header
+    Header,
+    Section
   }
 })
 </script>
