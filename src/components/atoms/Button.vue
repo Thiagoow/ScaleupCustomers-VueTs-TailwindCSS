30a6c35dd60970cdf4f1f8bf6047eb46b887e179
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-26 17:47:26 -0300

    Section component created

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :href="link">
+    <button
+      class="bg-firstColor hover:opacity-70 text-titleColor font-semibold rounded-lg w-fit p-4 mx-auto md:mx-0"
+    >
+      {{ text }}
+    </button>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
---
+++ src/components/organisms/Section.vue
@@ -0,0 +1,55 @@
+<template>
+  <section
+    class="flex flex-col lg:flex-row items-center justify-center my-28 gap-12"
+    :class="reverse && 'flex-col-reverse lg:flex-row-reverse'"
+  >
+    <div class="text-center md:text-left grid gap-6 max-w-lg">
+      <h2 class="text-titleColor text-2xl font-bold">
+        {{ title }}
+      </h2>
+
+      <p class="text-txtColor">
+        {{ text }}
+      </p>
+
+      <Button text="Contratar agora" link="#contract" />
+    </div>
+
+    <img
+      :alt="alt"
+      :src="`../../assets/img/${image}`"
+      class="w-1/2 sm:w-7/12 lg:w-[35%]"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from '../atoms/Button.vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    },
+    reverse: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: { Button }
+})
+</script>
