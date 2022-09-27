af69c3adc283d7c23ab4015c478e53d80ad2bfaf
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-27 18:57:49 -0300

    Advantages & cards created

---
+++ src/components/atoms/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid bg-containerColor p-10 gap-4 rounded-lg hover:shadow-filt">
+    <h1 class="text-3xl text-titleColor font-semibold">
+      {{ title }}
+    </h1>
+
+    <p class="text-txtColor max-w-[14rem]">
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Card',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
---
+++ src/components/molecules/Advantages.vue
@@ -0,0 +1,22 @@
+<template>
+  <section
+    class="flex flex-col md:flex-row my-32 gap-10 text-center w-fit mx-auto"
+  >
+    <Card title="150+" text="Criadores a tua disposição" />
+
+    <Card title="4.000+" text="Horas de conteúdo exclusivo" />
+
+    <Card title="10x" text="Mais engajamento dos alunos" />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from '../atoms/Card.vue'
+
+export default defineComponent({
+  components: {
+    Card
+  }
+})
+</script>
