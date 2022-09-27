840a3d817c07f51e05710428772107a35b87f44c
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-26 23:32:09 -0300

    Header set as sticky :DD

--- src/components/molecules/Header.vue
+++ src/components/molecules/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="flex relative z-50 justify-between mx-5 mt-10 xs:mx-14 lg:!mx-auto lg:max-w-4xl self-center"
+    class="flex top-0 sticky z-50 py-7 justify-between bg-bodyColor mx-5 xs:mx-14 lg:!mx-auto lg:max-w-5xl"
   >
     <Logo />
 
@@ -14,7 +14,7 @@
 
       <div :class="menuOpen ? 'flex' : 'hidden'" class="md:flex">
         <ul
-          class="flex flex-col absolute md:!relative right-0 2xs:right-5 top-16 py-5 rounded-lg bg-containerColor text-titleColor text-center font-semibold md:flex-row md:top-0 md:!right-0 md:space-x-8 lg:space-x-10 md:py-0 md:bg-transparent"
+          class="flex flex-col absolute md:!relative right-0 2xs:right-5 top-24 py-5 rounded-lg bg-containerColor text-titleColor text-center font-semibold md:flex-row md:top-0 md:!right-0 md:space-x-8 lg:space-x-10 md:py-0 md:bg-transparent"
         >
           <NavLink text="Produto" link="#product" />
 
