3e2c949b6621757d17ec4613796435e845886c8e
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-27 19:23:04 -0300

    Testimonial added

---
+++ src/components/organisms/Testimonial.vue
@@ -0,0 +1,47 @@
+<template>
+  <section
+    class="bg-bannerColor rounded-xl max-w-2xl mx-auto lg:max-w-[60rem] my-40"
+  >
+    <div class="items-center flex flex-col">
+      <div class="flex">
+        <img
+          src="../../assets/img/director.png"
+          alt="Diretor de escola"
+          class="hidden lg:flex max-w-[18rem] ml-8 lg:pt-14"
+        />
+
+        <div class="flex flex-col text-left p-10 lg:pt-20">
+          <img
+            src="../../assets/img/quotes.svg"
+            class="max-w-[2rem]"
+            alt="Ilustração de aspas duplas"
+          />
+
+          <p class="text-txtColor mb-8 mt-6 mx-0 max-w-xl">
+            A Scaleup foi a responsável por integrar nosso sistema de ensino á
+            modalidade de ensino a distância. Através da plataforma, conseguimos
+            oferecer cursos de qualidade para nossos alunos, com um custo menor
+            e com uma plataforma de fácil utilização.
+          </p>
+
+          <div class="flex max-h-14 max-w-14">
+            <img
+              src="../../assets/img/directorAvatar.png"
+              alt="logo objetivo"
+              class="max-w-[4rem] lg:hidden"
+            />
+            <div class="ml-5 lg:m-0">
+              <p class="text-titleColor font-semibold">Paulo Magaglio</p>
+              <p class="text-txtColor">Diretor de escola</p>
+            </div>
+          </div>
+
+          <div class="flex self-end gap-10 mt-6 sm:mt-0">
+            <button class="opacity-40">Anterior</button>
+            <button class="text-firstColor">Próximo</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
