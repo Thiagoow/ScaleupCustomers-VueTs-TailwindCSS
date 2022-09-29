259d91f7950849bd2dd0f73884e41954d7e2bf8a
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-28 23:53:18 -0300

    Created contract info component

---
+++ src/components/atoms/Info.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="">
+    <h3 class="text-txtColor mb-3">{{ title }}</h3>
+
+    <a
+      :href="link"
+      class="inline-flex items-center text-titleColor hover:text-firstColor"
+    >
+      <img :src="icon" :alt="alt" class="max-w-[1rem] mr-2" />
+      {{ text }}
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
