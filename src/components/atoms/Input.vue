01249f1b25ba9abfcadc58816ab6dd752831452a
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-28 23:53:36 -0300

    Created contract input component

---
+++ src/components/atoms/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="relative h-12">
+    <input :type="type" placeholder="" class="contractInput focus:labelUp" />
+    <label for="" class="labelDown">
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
